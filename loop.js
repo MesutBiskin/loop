0fd333adc519533de004b58542ac2121c075b0f1
Mesut Biskin <msbiskin@gmail.com>
2023-10-10 10:52:04 -0400

    Demo for how to make for loop

---
+++ loop.js
@@ -0,0 +1,29 @@
+//  Say hello 3 times
+for(let i=0;i<3;i++){
+    console.log('hello')
+}
+
+
+//  soru : 0 dan 3 e kadar 0,1,2,3 ekrana yazdir
+//for(let i=0; i < 4; i++) bu sekilde de yazabiliriz
+
+for(let i=0;i <=3; i++){
+    console.log(i)
+}
+
+const people = ['Todd','Andrew','Derek']
+//make a loop that sats hello to each person in the array
+ 
+for (let i=0;i< people.length;i++){
+    console.log("Hello "+ people[i])
+}
+
+
+const videos = ['cats','news','games','sports']
+// Console log each of these videos to the screen
+// Make sure it works for any lenght of array
+
+for(let i=0; i<videos.length ;i++){
+console.log(videos[i])
+
+}
